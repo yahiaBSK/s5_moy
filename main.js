b1726c33c747c16ef3b4e7f2173e772795012e4e
yahiabsk <boussouakyahia@gmail.com>
2022-03-01 01:01:30 +0100

    hi

--- main.js
+++ main.js
@@ -42,9 +42,16 @@ let degree = document.querySelector(".degree")
 // credit
 let credit = document.querySelector(".credit")
 
-
-
-
+// module moy
+let trans_moy_p = document.querySelector(".trans-moy")
+let mach_moy_p = document.querySelector(".mach-moy")
+let schema_moy_p = document.querySelector(".schema-moy")
+let syst_moy_p = document.querySelector(".syst-moy")
+let const_moy_p = document.querySelector(".const-moy")
+let elect_moy_p = document.querySelector(".elect-moy")
+let produc_moy_p = document.querySelector(".produc-moy")
+let mater_moy_p = document.querySelector(".mater-moy")
+let secur_moy_p = document.querySelector(".secur-moy")
 
 
 
@@ -54,75 +61,93 @@ let credit = document.querySelector(".credit")
 function calcule(){
   
   // transfere moy
-  let trans_moy = ((+trans_td.value*0.4)+(+trans_ex.value*0.6))*2
+  let trans_moy = ((+trans_td.value*0.4)+(+trans_ex.value*0.6))
   if (trans_moy>=10) {
     var trans_credit = 4
+    trans_moy_p.classList = "span-moy-good"
   }else{
     var trans_credit = 0
+    trans_moy_p.classList = "span-moy-bad"
   }
   
   // machines moy
-  let mach_moy = ((+mach_td.value*0.4)+(+mach_ex.value*0.6))*2
+  let mach_moy = ((+mach_td.value*0.4)+(+mach_ex.value*0.6))
   if (mach_moy>=10) {
     var mach_credit = 4
+    mach_moy_p.classList = "span-moy-good"
   }else{
     var mach_credit = 0
+    mach_moy_p.classList = "span-moy-bad"
   }
   
-  // shema moy
-  let schema_moy = ((+schema_ex.value*0.6)+(+schema_tp.value*0.4))*2
+  // schema moy
+  let schema_moy = ((+schema_ex.value*0.6)+(+schema_tp.value*0.4))
   if (schema_moy>=10) {
     var schema_credit = 3
+    schema_moy_p.classList = "span-moy-good"
   }else{
     var schema_credit = 0
+    schema_moy_p.classList = "span-moy-bad"
   }
   
   // systeme moy
-  let syst_moy = ((+syst_td.value*0.4)+(+syst_ex.value*0.6))*2
+  let syst_moy = ((+syst_td.value*0.4)+(+syst_ex.value*0.6))
   if (syst_moy >= 10) {
     var syst_credit = 4
+    syst_moy_p.classList = "span-moy-good"
   } else {
     var syst_credit = 0
+    syst_moy_p.classList = "span-moy-bad"
   }
   
   // const moy
-  let const_moy = (+const_ex.value)*1
+  let const_moy = (+const_ex.value)
   if (const_moy >= 10) {
     var const_credit = 2
+    const_moy_p.classList = "span-moy-good"
   } else {
     var const_credit = 0
+    const_moy_p.classList = "span-moy-bad"
   }
   
   // elect moy
-  let elect_moy = ((+elect_td.value*0.4)+(+elect_ex.value*0.6))*2
+  let elect_moy = ((+elect_td.value*0.4)+(+elect_ex.value*0.6))
   if (elect_moy >= 10) {
     var elect_credit = 4
+    elect_moy_p.classList = "span-moy-good"
   } else {
     var elect_credit = 0
+    elect_moy_p.classList = "span-moy-bad"
   }
   
   // production moy
-  let produc_moy = (+produc_ex.value)*1
+  let produc_moy = (+produc_ex.value)
   if (produc_moy >= 10) {
     var produc_credit = 1
+    produc_moy_p.classList = "span-moy-good"
   } else {
     var produc_credit = 0
+    produc_moy_p.classList = "span-moy-bad"
   }
   
   // materiaux moy
-  let mater_moy = (+mater_ex.value)*1
+  let mater_moy = (+mater_ex.value)
   if (mater_moy >= 10) {
     var mater_credit = 1
+    mater_moy_p.classList = "span-moy-good"
   } else {
     var mater_credit = 0
+    mater_moy_p.classList = "span-moy-bad"
   }
   
   // securiee moy
-  let secur_moy = (+secur_ex.value)*1
+  let secur_moy = (+secur_ex.value)
   if (secur_moy >= 10) {
     var secur_credit = 1
+    secur_moy_p.classList = "span-moy-good"
   } else {
     var secur_credit = 0
+    secur_moy_p.classList = "span-moy-bad"
   }
   
   
@@ -148,11 +173,11 @@ function calcule(){
 
   
   // calcule moy uef-2-d-t
-  let moy_uef = (elect_moy+mach_moy+const_moy)/5
-  let moy_uef2 = (trans_moy+syst_moy)/4
-  let moy_uem = (+elect_tp.value+ +mach_tp.value+ +syst_tp.value+schema_moy)/5
-  let moy_ued = (produc_moy+mater_moy)/2
-  let moy_uet = secur_moy/1
+  let moy_uef = (elect_moy*2+mach_moy*2+const_moy*1)/5
+  let moy_uef2 = (trans_moy*2+syst_moy*2)/4
+  let moy_uem = (+elect_tp.value+ +mach_tp.value+ +syst_tp.value+schema_moy*2)/5
+  let moy_ued = (produc_moy*1+mater_moy*1)/2
+  let moy_uet = (secur_moy*1)/1
   
   
   
@@ -193,7 +218,16 @@ function calcule(){
     opacitys(credit)
   }
   
-  
+  // calcule modules moyenne
+  mach_moy_p.innerHTML = mach_moy
+  trans_moy_p.innerHTML = trans_moy
+  schema_moy_p.innerHTML = schema_moy
+  syst_moy_p.innerHTML = syst_moy
+  const_moy_p.innerHTML = const_moy
+  elect_moy_p.innerHTML = elect_moy
+  produc_moy_p.innerHTML = produc_moy
+  mater_moy_p.innerHTML = mater_moy
+  secur_moy_p.innerHTML = secur_moy
 }
 
 
