3a6aea0619aab1499a9ddc611a172d156975a79a
yahia boussouak <boussouakyahia@gmail.com>
2022-09-09 00:53:35 +0100

    &

---
+++ s5/main.js
@@ -0,0 +1,19 @@
+let title_h1 = document.querySelector('.title h1')
+// title_h2_1 = document.querySelector('.title #h2_1')
+// title_h2_2 = document.querySelector('.title #h2_2')
+btn_s5 = document.querySelector('#s5')
+// btn_s6 = document.querySelector('#s6')
+
+
+
+window.addEventListener('load', ()=>{
+	setTimeout(()=>{
+		
+		title_h1.style = "opacity: 1; transform: none";
+  	// title_h2_1.style = "opacity: 1; transform: none"
+	  // title_h2_2.style = "opacity: 1; transform: none"
+	  btn_s5.style = "opacity: 1;transform: scale(1)"
+	  // btn_s6.style = "opacity:1; transform: scale(1)"
+	
+	},200)
+})
